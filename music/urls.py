c5e5efdf43306d27664852d18d4c94d363b15522
menesesjmmc <menesesjmmc@gmail.com>
2017-01-25 16:58:38 +0000

    almost finished

--- music/urls.py
+++ music/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     # /music/
     url(r'^$', views.IndexView.as_view(), name='index'),  #if the user only requests music/ then index is loaded
 
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
     # /music/<album_id>/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
--- music/views.py
+++ music/views.py
@@ -33,3 +33,38 @@ class AlbumUpdate(UpdateView):
 class AlbumDelete(DeleteView):
     model = Album
     success_url = reverse_lazy('music:index')
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    #display blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    # process form data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False) #creates an object from the form, doesnt save to the database
+
+            #clean normalized data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            #returns User Object if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+
+                if user.is_active:  #account is not banned
+                    login(request, user)
+                    return redirect('music:index')
+
+        return render(request, self.template_name, {'form': form})
